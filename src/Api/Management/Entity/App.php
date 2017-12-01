43831edc15bd5c50cd35e1406eefa39990e9c8b9
Dezső Biczó <mxr576@gmail.com>
2017-12-01 10:25:47 +0100

    Changing idField of apps.

--- src/Api/Management/Entity/App.php
+++ src/Api/Management/Entity/App.php
@@ -43,7 +43,9 @@ abstract class App extends Entity implements AppInterface
      */
     public function idProperty(): string
     {
-        return 'appId';
+        // Even though the 'appId' should be the primary id field of an app, the majority of the Edge endpoints are
+        // expecting the app name as a parameter. Ex.: All C.R.U.D. operations on developer apps.
+        return parent::idProperty();
     }
 
     /**
