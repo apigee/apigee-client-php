33a57c65e49dfe1dfb6e3c383b219414a67e4a4b
Dezső Biczó <mxr576@gmail.com>
2018-10-29 14:04:31 +0100

    Ensure the organization name is available.

--- src/Controller/PaginationHelperTrait.php
+++ src/Controller/PaginationHelperTrait.php
@@ -32,6 +32,7 @@ trait PaginationHelperTrait
     use ClientAwareControllerTrait;
     use BaseEndpointAwareControllerTrait;
     use OrganizationControllerAwareTrait;
+    use OrganizationAwareControllerTrait;
 
     /**
      * @inheritdoc
@@ -39,9 +40,9 @@ trait PaginationHelperTrait
     public function createPager(int $limit = 0, ?string $startKey = null): PagerInterface
     {
         /** @var \Apigee\Edge\Api\Management\Entity\OrganizationInterface $organization */
-        $organization = $this->getOrganizationController()->load($this->organization);
+        $organization = $this->getOrganizationController()->load($this->getOrganisationName());
         if (!$organization->getPropertyValue('features.isCpsEnabled')) {
-            throw new CpsNotEnabledException($this->organization);
+            throw new CpsNotEnabledException($this->getOrganisationName());
         }
 
         // Create an anonymous class here because this class should not exist and be in use
