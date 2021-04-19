2087b07553e43e096b3497a9afcccbf9210577be
phdhiren <71828912+phdhiren@users.noreply.github.com>
2021-04-20 04:03:39 +0530

    Bump version to 2.0.7 and changelog (#137)
    * Updated client version in ClientInterface 2.0.7
    
    * Updated changelog for 2.0.7.
    
    * Updated changelog for 2.0.7.
    
    * Updated changelog for 2.0.7.

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -44,7 +44,7 @@ interface ClientInterface extends HttpClient
      */
     public const HYBRID_ENDPOINT = 'https://apigee.googleapis.com/v1';
 
-    public const VERSION = '2.0.6';
+    public const VERSION = '2.0.7';
 
     /**
      * Allows access to the last request, response and exception.
