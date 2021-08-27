582b6dafaed601ad88164e7feb8f16acd76e22df
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2021-08-27 10:27:13 +0530

    ApigeeX code for Billing type (#168)
    * Billing Type entity

---
+++ src/Api/ApigeeX/Controller/BillingTypeControllerInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Controller;
+
+use Apigee\Edge\Api\ApigeeX\Entity\BillingTypeInterface;
+use Apigee\Edge\Controller\EntityControllerInterface;
+use Apigee\Edge\Controller\EntityLoadOperationControllerInterface;
+
+/**
+ * Interface BillingTypeControllerInterface.
+ *
+ * TODO: Add reference documentation link
+ */
+interface BillingTypeControllerInterface extends
+    EntityControllerInterface,
+    EntityLoadOperationControllerInterface
+{
+    /**
+     * Gets the billingtype.
+     *
+     * @return \Apigee\Edge\Api\ApigeeX\Entity\BillingTypeInterface[]
+     */
+    public function getAllBillingDetails(): BillingTypeInterface;
+
+    /**
+     * Update the billing type of the developer.
+     *
+     * @param $billingtype
+     *   Billing type that is switched to.
+     */
+    public function updateBillingType($billingtype): BillingTypeInterface;
+}
---
+++ src/Api/ApigeeX/Denormalizer/DeveloperBillingTypeDenormalizer.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Denormalizer;
+
+use Apigee\Edge\Api\ApigeeX\Entity\DeveloperBillingType;
+
+class DeveloperBillingTypeDenormalizer extends BillingTypeDenormalizer
+{
+    /**
+     * Fully qualified class name of the developer billing type.
+     *
+     * @var string
+     */
+    protected $developerBillinTypeClass = DeveloperBillingType::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        return parent::denormalize($data, $this->developerBillinTypeClass, $format, $context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if (parent::supportsDenormalization($data, $type, $format)) {
+            return true;
+        }
+
+        return false;
+    }
+}
