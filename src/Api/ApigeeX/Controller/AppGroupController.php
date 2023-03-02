1d96ea17bfe4e714c9719784762a2b3a5df576e0
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2023-03-02 14:17:10 +0530

    Appgroup entity (#246)
    * Files for the appgroup entity

---
+++ src/Api/ApigeeX/Controller/AppGroupController.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Controller;
+
+use Apigee\Edge\Api\ApigeeX\Entity\AppGroup;
+use Apigee\Edge\Api\ApigeeX\Serializer\AppGroupSerializer;
+use Apigee\Edge\Api\Management\Controller\AttributesAwareEntityControllerTrait;
+use Apigee\Edge\ClientInterface;
+use Apigee\Edge\Controller\EntityController;
+use Apigee\Edge\Controller\EntityCrudOperationsControllerTrait;
+use Apigee\Edge\Controller\EntityListingControllerTrait;
+use Apigee\Edge\Controller\StatusAwareEntityControllerTrait;
+use Apigee\Edge\Serializer\EntitySerializerInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Class AppGroupController.
+ */
+class AppGroupController extends EntityController implements AppGroupControllerInterface
+{
+    use AttributesAwareEntityControllerTrait;
+    use EntityCrudOperationsControllerTrait;
+    use EntityListingControllerTrait;
+    use StatusAwareEntityControllerTrait;
+
+    /**
+     * AppGroupController constructor.
+     *
+     * @param string $organization
+     * @param \Apigee\Edge\ClientInterface $client
+     * @param \Apigee\Edge\Serializer\EntitySerializerInterface|null $entitySerializer
+     * @param \Apigee\Edge\Api\Management\Controller\OrganizationControllerInterface|null $organizationController
+     */
+    public function __construct(string $organization, ClientInterface $client, ?EntitySerializerInterface $entitySerializer = null)
+    {
+        $entitySerializer = $entitySerializer ?? new AppGroupSerializer();
+        parent::__construct($organization, $client, $entitySerializer);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEntities(): array
+    {
+        $uri = $this->getBaseEndpointUri();
+        $response = $this->client->get($uri);
+        $responseArray = $this->responseToArray($response);
+        // Ignore entity type key from response.
+        $responseArray = reset($responseArray);
+
+        // Appgroup can be empty.
+        if (empty($responseArray)) {
+            return [];
+        }
+
+        return $this->responseArrayToArrayOfEntities($responseArray);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBaseEndpointUri(): UriInterface
+    {
+        return $this->client->getUriFactory()->createUri("/organizations/{$this->organization}/appgroups");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEntityClass(): string
+    {
+        return AppGroup::class;
+    }
+}
