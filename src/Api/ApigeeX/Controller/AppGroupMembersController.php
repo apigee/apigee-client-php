8757acad3e312743ffdac440ffeec8be433e1062
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2023-06-13 18:03:11 +0530

    Implemented AppGroup membership feature (#268)

---
+++ src/Api/ApigeeX/Controller/AppGroupMembersController.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Controller;
+
+use Apigee\Edge\Api\ApigeeX\Serializer\AppGroupMembershipSerializer;
+use Apigee\Edge\Api\ApigeeX\Structure\AppGroupMembership;
+use Apigee\Edge\ClientInterface;
+use Apigee\Edge\Controller\AbstractController;
+use Apigee\Edge\Controller\OrganizationAwareControllerTrait;
+use Apigee\Edge\Structure\AttributesProperty;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Allows to manage appgroup memberships.
+ */
+class AppGroupMembersController extends AbstractController implements AppGroupMembersControllerInterface
+{
+    use AppGroupAwareControllerTrait;
+    use OrganizationAwareControllerTrait;
+
+    /**
+     * @var \Apigee\Edge\Serializer\EntitySerializerInterface
+     */
+    protected $serializer;
+
+    /** @var string */
+    protected $organization;
+
+    /**
+     * AppGroupMembersController constructor.
+     *
+     * @param string $appGroup
+     * @param string $organization
+     * @param \Apigee\Edge\ClientInterface $client
+     */
+    public function __construct(string $appGroup, string $organization, ClientInterface $client)
+    {
+        parent::__construct($client);
+        $this->appGroup = $appGroup;
+        $this->organization = $organization;
+        $this->serializer = new AppGroupMembershipSerializer();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMembers(): AppGroupMembership
+    {
+        $response = $this->client->get($this->getBaseEndpointUri());
+
+        return $this->serializer->denormalize($this->responseToArray($response), AppGroupMembership::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMembers(AppGroupMembership $members): AppGroupMembership
+    {
+        $members = $this->serializer->normalize($members);
+        $apigeeReservedMembers = new AttributesProperty();
+
+        // Adding the new members into the attribute.
+        $apigeeReservedMembers->add('__apigee_reserved__developer_details', json_encode($members));
+        $response = $this->client->put(
+            $this->getBaseEndpointUri(),
+            (string) json_encode((object) [
+                'attributes' => $this->serializer->normalize($apigeeReservedMembers),
+            ])
+        );
+
+        return $this->serializer->denormalize($this->responseToArray($response), AppGroupMembership::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * TODO: Remove removeMember method as it is not used for AppGroup membership
+     * As we are storing the Team members details inside __apigee_reserved__developer_details
+     * attribute, we dont have separate API to delete the members from the attribute.
+     * So we update the __apigee_reserved__developer_details attribute json at setReservedMembership().
+     */
+    public function removeMember(string $email): void
+    {
+        $encoded = rawurlencode($email);
+        $this->client->delete($this->getBaseEndpointUri()->withPath("{$this->getBaseEndpointUri()->getPath()}/{$encoded}"));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrganisationName(): string
+    {
+        return $this->organization;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBaseEndpointUri(): UriInterface
+    {
+        return $this->client->getUriFactory()->createUri("/organizations/{$this->organization}/appgroups/{$this->appGroup}");
+    }
+}
---
+++ src/Api/ApigeeX/Controller/AppGroupMembersControllerInterface.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Api\ApigeeX\Controller;
+
+use Apigee\Edge\Api\ApigeeX\Structure\AppGroupMembership;
+
+/**
+ * Interface AppGroupMembersControllerInterface.
+ */
+interface AppGroupMembersControllerInterface extends AppGroupAwareControllerInterface
+{
+    /**
+     * List all developers associated with a appgroup.
+     *
+     * @return \Apigee\Edge\Api\ApigeeX\Structure\AppGroupMembership
+     *   Array of developers with their optional roles in the appgroup.
+     */
+    public function getMembers(): AppGroupMembership;
+
+    /**
+     * Set (add/update/remove) members of a appgroup.
+     *
+     * WARNING! If you pass en empty membership object you remove all developers
+     * from the appgroup.
+     *
+     * @param \Apigee\Edge\Api\ApigeeX\Structure\AppGroupMembership $members
+     *   Membership object with the changes to be applied.
+     *
+     * @return \Apigee\Edge\Api\ApigeeX\Structure\AppGroupMembership
+     *   Membership object with the applied changes, it does not contain all
+     *   members. Use getMembers() to retrieve them.
+     */
+    public function setMembers(AppGroupMembership $members);
+
+    /**
+     * Removes a developer from a appgroup.
+     *
+     * @param string $email
+     *   Email address of a developer.
+     */
+    public function removeMember(string $email): void;
+}
