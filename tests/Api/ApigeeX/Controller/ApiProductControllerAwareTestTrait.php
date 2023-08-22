64d80ba9207efe61bf8bce801844807100e20643
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2023-08-22 20:46:48 +0530

    Testcase for AppGroup apps:2x (#299)

---
+++ tests/Api/ApigeeX/Controller/ApiProductControllerAwareTestTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\Edge\Tests\Api\ApigeeX\Controller;
+
+use Apigee\Edge\Api\ApigeeX\Controller\ApiProductController;
+use Apigee\Edge\ClientInterface;
+use Apigee\Edge\Tests\Test\Controller\DefaultAPIClientAwareTrait;
+use Apigee\Edge\Tests\Test\Controller\defaultTestOrganizationAwareTrait;
+use Apigee\Edge\Tests\Test\Controller\EntityControllerTester;
+use Apigee\Edge\Tests\Test\Controller\EntityControllerTesterInterface;
+
+trait ApiProductControllerAwareTestTrait
+{
+    use DefaultTestOrganizationAwareTrait;
+    use DefaultAPIClientAwareTrait;
+
+    /**
+     * @param \Apigee\Edge\ClientInterface|null $client
+     *
+     * @return \Apigee\Edge\Tests\Test\Controller\EntityControllerTesterInterface|\Apigee\Edge\Api\ApigeeX\Controller\ApiProductControllerInterface
+     */
+    protected static function apiProductController(ClientInterface $client = null): EntityControllerTesterInterface
+    {
+        $client = $client ?? static::defaultAPIClient();
+
+        return new EntityControllerTester(new ApiProductController(static::defaultTestOrganization($client), $client));
+    }
+}
