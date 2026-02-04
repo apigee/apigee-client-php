ad85bd16521aa33f40116614015b5f94aaf0900f
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2026-02-04 14:21:14 +0530

    Fix PHPCS issues

--- tests/PropertyAccess/PropertyAccessorDecoratorTest.php
+++ tests/PropertyAccess/PropertyAccessorDecoratorTest.php
@@ -205,12 +205,12 @@ class PropertyAccessorDecoratorTest extends PropertyAccessorTest
                 $shouldBeAString,
                 ['shouldBeAString', UnexpectedValueException::class, '/Invalid value returned for shouldBeAString property on instance of class@anonymous.* class. Expected type "string", got "stdClass".$/'],
             ];
-        } else {
-            return [
-                $shouldBeAString,
-                ['shouldBeAString', TypeError::class, '/Return value must be of type string, stdClass returned/'],
-            ];
         }
+
+        return [
+            $shouldBeAString,
+            ['shouldBeAString', TypeError::class, '/Return value must be of type string, stdClass returned/'],
+        ];
     }
 
     public function exceptionsToGetOnSetValue(): array
@@ -220,11 +220,11 @@ class PropertyAccessorDecoratorTest extends PropertyAccessorTest
             return [
                 ['shouldBeAStringArray', [null], InvalidArgumentException::class, '/^Expected argument of type "string", "null" given/'],
             ];
-        } else {
-            return [
-                ['shouldBeAStringArray', [null], TypeError::class, '/Argument #1 must be of type string, null given/'],
-            ];
         }
+
+        return [
+            ['shouldBeAStringArray', [null], TypeError::class, '/Argument #1 must be of type string, null given/'],
+        ];
     }
 
     /* * COMPATIBILITY OVERRIDES (PHPUnit 9+ vs Symfony 7.4+) *
