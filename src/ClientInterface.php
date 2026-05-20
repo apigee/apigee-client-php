8022eba144f66564f156fc3ac8e5288957bfd154
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2026-05-20 13:15:15 +0530

    Bump version to 3.0.12 and changelog.md file changes (#475)

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -63,7 +63,7 @@ interface ClientInterface extends HttpClient
      */
     public const APIGEE_ON_GCP_ENDPOINT = 'https://apigee.googleapis.com/v1';
 
-    public const VERSION = '3.0.11';
+    public const VERSION = '3.0.12';
 
     /**
      * Allows access to the last request, response and exception.
